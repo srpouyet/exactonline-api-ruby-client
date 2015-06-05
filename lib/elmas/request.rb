f3c6d988e2dcb1476e68a174bb8ae92b5fb16314
Marthyn <Marthyn@live.nl>
2015-06-05 10:12:28 +0200

    Remove uneccesary user agent header

--- lib/elmas/request.rb
+++ lib/elmas/request.rb
@@ -36,7 +36,6 @@ module Elmas
       headers = {}
       headers["Content-Type"] = "application/#{response_format}"
       headers["Accept"] = "application/#{response_format}"
-      headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
       headers["Authorization"] = "Bearer #{access_token}" if access_token
       headers
     end
