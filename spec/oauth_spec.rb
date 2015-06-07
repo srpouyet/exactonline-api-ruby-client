219ebdb2716b5f7d9b83a502e108e3c3bbc34e5b
Dunya Kirkali <dunyakirkali@gmail.com>
2015-06-07 13:49:37 +0200

    add placeholder specs

--- spec/oauth_spec.rb
+++ spec/oauth_spec.rb
@@ -1,11 +1,15 @@
 require "spec_helper"
 
+# TODO: (dunyakirkali) write better specs
 describe Elmas::OAuth do
   describe "#" do
     it "authorize" do
-      stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/?client_id=client_id&redirect_uri=redirect_uri&response_type=code").
-               with(:headers => {'Accept'=>'*/*', 'Accept-Charset'=>'ISO-8859-1,utf-8;q=0.7,*;q=0.7', 'Accept-Encoding'=>'gzip,deflate,identity', 'Accept-Language'=>'en-us,en;q=0.5', 'Connection'=>'keep-alive', 'Host'=>'start.exactonline.nl', 'Keep-Alive'=>'300', 'User-Agent'=>'Mechanize/2.7.3 Ruby/2.1.2p95 (http://github.com/sparklemotion/mechanize/)'}).
-               to_return(:status => 200, :body => "", :headers => {})
+      stream = File.read("spec/exact_login.html")
+      login_params = { client_id: "client_id", redirect_uri: "redirect_uri", response_type: "code" }
+      stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/").with(query: login_params).to_return(body: stream, headers: { content_type: "text/html" })
+      stub_request(:post, "https://start.exactonline.nl/api/oauth2/auth/?client_id=24509072-f819-40ff-b888-4cd545985392&redirect_uri=https://www.getpostman.com/oauth2/callback&response_type=code")
+      stub_request(:post, "https://start.exactonline.nl/api/oauth2/token")
+      response = Elmas.authorize("ExactUsernae", "ExactPassword")
       expect(response.status).to eq(200)
     end
   end
