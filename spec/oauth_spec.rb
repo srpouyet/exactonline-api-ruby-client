6fa0c3af56bc0084696c445d20e04d7532324171
Dunya Kirkali <dunyakirkali@gmail.com>
2015-06-07 20:14:22 +0200

    organize fixtures

--- spec/oauth_spec.rb
+++ spec/oauth_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 describe Elmas::OAuth do
   describe "#" do
     it "authorize" do
-      stream = File.read("spec/exact_login.html")
+      stream = File.read("spec/fixtures/exact_login.html")
       login_params = { client_id: "client_id", redirect_uri: "redirect_uri", response_type: "code" }
       stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/").with(query: login_params).to_return(body: stream, headers: { content_type: "text/html" })
       stub_request(:post, "https://start.exactonline.nl/api/oauth2/auth/?client_id=24509072-f819-40ff-b888-4cd545985392&redirect_uri=https://www.getpostman.com/oauth2/callback&response_type=code")
