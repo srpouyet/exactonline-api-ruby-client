dc233b3311b85b051e4128cc929b93a30d3feb02
Marthyn <Marthyn@live.nl>
2015-07-31 11:43:55 +0200

    Create errors log directory if not exists

--- lib/elmas/log.rb
+++ lib/elmas/log.rb
@@ -3,6 +3,8 @@ require "logger"
 module Elmas
   module Log
     def logger
+      dir = File.dirname("./tmp/errors.log")
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
       Logger.new("./tmp/errors.log", "daily")
     end
 
