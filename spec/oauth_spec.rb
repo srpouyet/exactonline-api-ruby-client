3081b5426262efe2c9a56c33c1c468e560d96776
Marthyn <Marthyn@live.nl>
2015-08-26 13:46:37 +0200

    Fix oauth spec

--- spec/oauth_spec.rb
+++ spec/oauth_spec.rb
@@ -4,9 +4,14 @@ require "spec_helper"
 describe Elmas::OAuth do
   describe "#" do
     it "authorize" do
+      client_id = "24509072-f819-40ff-b888-4cd545985392"
+      Elmas.configure do |config|
+        config.client_id = client_id
+      end
       stream = File.read("spec/fixtures/exact_login.html")
-      login_params = { client_id: "client_id", redirect_uri: "redirect_uri", response_type: "code" }
-      stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/").with(query: login_params).to_return(body: stream, headers: { content_type: "text/html" })
+      login_params = { client_id: client_id, redirect_uri: "https://www.getpostman.com/oauth2/callback", response_type: "code" }
+      stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/?client_id=24509072-f819-40ff-b888-4cd545985392&redirect_uri=redirect_uri&response_type=code").to_return(body: stream, headers: { content_type: "text/html" })
+      stub_request(:get, "https://start.exactonline.nl/api/oauth2/auth/?client_id=24509072-f819-40ff-b888-4cd545985392&redirect_uri=https://www.getpostman.com/oauth2/callback&response_type=code").to_return(body: stream, headers: { content_type: "text/html" })
       stub_request(:post, "https://start.exactonline.nl/api/oauth2/auth/?client_id=24509072-f819-40ff-b888-4cd545985392&redirect_uri=https://www.getpostman.com/oauth2/callback&response_type=code")
       stub_request(:post, "https://start.exactonline.nl/api/oauth2/token")
       response = Elmas.authorize("ExactUsernae", "ExactPassword")
