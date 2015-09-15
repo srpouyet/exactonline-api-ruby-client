d587d0b75816f3bc927c91f436a585ff57f2023e
Marthyn <Marthyn@live.nl>
2015-09-15 09:38:48 +0200

    Add bankentry

---
+++ lib/elmas/resources/bank_entry.rb
@@ -0,0 +1,22 @@
+module Elmas
+  class BankEntry
+    # A sales entry needs multiple sales entry lines
+    # It should also have a journal id and a contact id who ordered it
+    include Elmas::Resource
+
+    def base_path
+      "financialtransaction/BankEntry"
+    end
+
+    def mandatory_attributes
+      [:journal_code, :bank_entry_lines]
+    end
+
+    def other_attributes
+      [
+        :currency, :bank_statement_document, :closing_balance_FC, :entry_number,
+        :financial_period, :financial_year, :openening_balance_FC
+      ]
+    end
+  end
+end
