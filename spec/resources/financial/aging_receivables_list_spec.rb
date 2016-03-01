582db3674a540386728e788448a95e14d6769b73
Peter Duijnstee <peter@commuun.nl>
2016-03-01 14:51:43 +0100

    Fix specs too...

--- spec/resources/financial/aging_receivables_list_spec.rb
+++ spec/resources/financial/aging_receivables_list_spec.rb
@@ -19,19 +19,19 @@ describe Elmas::AgingReceivablesList do
   context "Applying filters" do
     resource = Elmas::AgingReceivablesList.new(id: '12abcdef-1234-1234-1234-123456abcdef')
     it "should apply ID filter for find" do
-      expect(Elmas).to receive(:get).with("financial/AgingReceivablesLists(guid'12abcdef-1234-1234-1234-123456abcdef')?")
+      expect(Elmas).to receive(:get).with("read/financial/AgingReceivablesList(guid'12abcdef-1234-1234-1234-123456abcdef')?")
       resource.find
     end
 
     it "should apply no filters for find_all" do
       resource = Elmas::AgingReceivablesList.new(type: 2)
-      expect(Elmas).to receive(:get).with("financial/AgingReceivablesLists?")
+      expect(Elmas).to receive(:get).with("read/financial/AgingReceivablesList?")
       resource.find_all
     end
 
     it "should apply given filters for find_by" do
       resource = Elmas::AgingReceivablesList.new(id: "12abcdef-1234-1234-1234-123456abcdef", type: "2")
-      expect(Elmas).to receive(:get).with("financial/AgingReceivablesLists?$filter=Type+eq+'2'&$filter=ID+eq+guid'12abcdef-1234-1234-1234-123456abcdef'")
+      expect(Elmas).to receive(:get).with("read/financial/AgingReceivablesList?$filter=Type+eq+'2'&$filter=ID+eq+guid'12abcdef-1234-1234-1234-123456abcdef'")
       resource.find_by(filters: [:type, :id])
     end
   end
