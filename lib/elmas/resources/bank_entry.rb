3f3dc844890aaf66f3ef590d0c9e31c3a7d1263d
LaurensN <laurensnutma@gmail.com>
2016-06-09 13:14:22 +0200

    Update bank_entry.rb

--- lib/elmas/resources/bank_entry.rb
+++ lib/elmas/resources/bank_entry.rb
@@ -13,7 +13,7 @@ module Elmas
     def other_attributes
       [
         :currency, :bank_statement_document, :closing_balance_FC, :entry_number,
-        :financial_period, :financial_year, :openening_balance_FC
+        :financial_period, :financial_year, :opening_balance_FC
       ]
     end
   end
