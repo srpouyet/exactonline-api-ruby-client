b7cfe4a38bec380338ec5be621193168edefda64
Daniel Senff <mail@danielsenff.de>
2016-07-13 14:00:30 +0200

    add AmountDC attribute to SharedSalesAttributes

--- lib/elmas/resources/shared_sales_attributes.rb
+++ lib/elmas/resources/shared_sales_attributes.rb
@@ -4,13 +4,13 @@ module Elmas
       :currency, :invoice_to, :invoice_to_contact_person, :invoice_date,
       :description, :document, :order_date, :ordered_by_contact_person,
       :tax_schedule, :order_number, :payment_condition, :payment_reference,
-      :remarks, :your_ref
+      :remarks, :your_ref, :amount_DC
     ].freeze
 
     SHARED_LINE_ATTRIBUTES = [
       :discount, :quantity, :amount_FC, :description, :VAT_code, :cost_center,
       :cost_unit, :VAT_percentage, :unit_code, :tax_schedule, :net_price, :notes,
-      :pricelist, :project
+      :pricelist, :project, :amount_DC
     ].freeze
   end
 end
